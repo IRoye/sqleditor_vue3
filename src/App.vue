b17df9509ab552f5e6be4046cb9f23c7e379af02
IRoye <1755431269@qq.com>
2024-08-09 00:18:33 +0800

    feat(编辑器): 使用组合式重写

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,30 @@
  * @Author: yuyi
  * @Date: 2024-08-05 23:02:29
  * @LastEditors: yuyi
- * @LastEditTime: 2024-08-06 00:13:51
+ * @LastEditTime: 2024-08-08 23:51:43
 -->
 <template>
 	<div class="layout">
-		<el-card style="width: 50%;">
-			<codemirror v-model="sqlCommand" :extensions="extensions" :autofocus="true" placeholder="请输入SQL" />
-		</el-card>
+		<sql-editor :command="SQLCommand" />
 	</div>
 </template>
 
 <script>
 import { SQL } from '@/constants/sql.js'
-import { sql } from '@codemirror/lang-sql'
-import { Codemirror } from 'vue-codemirror'
-import { oneDark } from '@codemirror/theme-one-dark'
+import SqlEditor from '@/components/SqlEditor/index.vue'
 export default {
 	name: 'SQLEditor',
 	components: {
-		Codemirror
+		SqlEditor
 	},
 	data() {
 		return {
-			sqlCommand: SQL,
-			extensions: [sql(), oneDark]
-		};
-	},
-	methods: {
+			SQLCommand: SQL
+		}
 	}
 };
 </script>
 <style lang="scss">
-.cm-editor {
-	width: 100%;
-	height: 460px;
-}
-
 .cm-focused {
 	outline: none !important;
 }
